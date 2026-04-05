c8384d413800a9d0ed708449c04635d8b44108b4
pedroluc4as <ordepsperandio@hotmail.com>
2026-04-05 13:40:27 -0300

    tomando forma

---
+++ algHammingInC/algoritmoHamming.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int opcao;
+    printf("---ALGORITMO DE HAMMING---");
+    printf("1. Codificar mensagem\n");
+    printf("2. Decodificar mensagem\n");
+    printf("0. Sair\n");
+    printf("Escolha uma opcao e digite o numero: ");
+    scanf("%d", &opcao);
+
+    switch(opcao) {
+        case 1:
+            printf("\nCodificando bits inseridos...\n");
+            
+            // A logica para pedir os 8 bits e calcular a paridade vai aqui
+            break;
+        case 2:
+            printf("\nDecodificando os bits...\n");
+            // A logica para receber os bits e decodificar vai aqui
+            break;
+        case 0:
+            printf("\nEncerrando o programa. Deus abencoe!\n");
+            break;
+        default:
+            printf("\nOpcao invalida!\n");
+    }
+
+    return 0;
+}
